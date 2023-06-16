4c0635cdf13e49da62a759a927d26ba064aeb0a1
rnrudejr9 <rnrudejr1234@naver.com>
2023-06-16 23:41:11 +0900

    Initial commit

---
+++ src/main/java/com/example/simpleproject/SimpleprojectApplication.java
@@ -0,0 +1,14 @@
+package com.example.simpleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleprojectApplication.class, args);
+    }
+
+
+}
